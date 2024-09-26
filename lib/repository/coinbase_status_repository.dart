7d93eee6aed9b270920a436c037b1434470c87ea
Richard Dewan <richarddewan@hotmail.com>
2024-09-26 10:27:40 +0700

    reconnect on error

--- lib/repository/coinbase_status_repository.dart
+++ lib/repository/coinbase_status_repository.dart
@@ -1,14 +1,13 @@
-
-
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:coinbase/api/coinbase_websocket.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-final coinbaseStatusRepositoryProvider = AutoDisposeStreamProvider<Map<String, dynamic>>((ref) {
+final coinbaseStatusRepositoryProvider =
+    AutoDisposeStreamProvider<Map<String, dynamic>>((ref) {
   final coinbaseWebsocket = ref.watch(coinbaseWebsocketProvider);
   final coinbaseStatusRepository = CoinbaseStatusRepository(coinbaseWebsocket);
 
@@ -18,48 +17,59 @@ final coinbaseStatusRepositoryProvider = AutoDisposeStreamProvider<Map<String, d
 class CoinbaseStatusRepository {
   final CoinbaseWebsocket _coinbaseWebsocket;
 
-  CoinbaseStatusRepository(this._coinbaseWebsocket){
+  CoinbaseStatusRepository(this._coinbaseWebsocket) {
     _init();
   }
 
   WebSocketChannel? _channel;
   bool _isDisposed = false;
   bool _isSubscribed = false;
-  final StreamController<Map<String, dynamic>> _streamController = StreamController<Map<String, dynamic>>();
+  final StreamController<Map<String, dynamic>> _streamController =
+      StreamController<Map<String, dynamic>>();
   Stream<Map<String, dynamic>> get stream => _streamController.stream;
 
   void _init() {
     _channel = _coinbaseWebsocket.connect();
-     _subscribeToStatus();
-     _listen();
+    _subscribeToStatus();
+    _listen();
   }
 
   void _subscribeToStatus() {
-   
     // check if we are not already disposed
     if (_isDisposed) return;
 
     final message = jsonEncode({
       "type": "subscribe",
-      "channels": [{ "name": "status"}],
+      "channels": [
+        {"name": "status"}
+      ],
     });
 
     // Subscribe to status
     _isSubscribed = true;
     // Send subscription message to websocket
     _channel?.sink.add(message);
-
   }
 
   void _listen() {
     if (_isDisposed) return;
 
-    _channel?.stream.listen((data) {
-      final jsonData = jsonDecode(data) as Map<String, dynamic>;      
-      _streamController.add(jsonData);      
-
-    },
-    cancelOnError: true,
+    _channel?.stream.listen(
+      (data) {
+        final jsonData = jsonDecode(data) as Map<String, dynamic>;
+        _streamController.add(jsonData);
+      },
+      onError: (e) {
+        _reconnect();
+      },
     );
+  }
+
+  void _reconnect() {
+    if (_isDisposed) return;
+
+    Future.delayed(const Duration(seconds: 3), () {
+      _init();
+    });
   }
 }
