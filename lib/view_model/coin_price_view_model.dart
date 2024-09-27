8f6a7a43b34de6f5c79a510899d3b82d93ae859e
Richard Dewan <richarddewan@hotmail.com>
2024-09-27 22:36:06 +0700

    coin price view model

---
+++ lib/view_model/coin_price_view_model.dart
@@ -0,0 +1,20 @@
+import 'package:coinbase/repository/coinbase_price_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+
+final coinPriceViewModelProvider = AutoDisposeFamilyNotifierProvider<CoinPriceViewModel, AsyncValue ,List<String>>(CoinPriceViewModel.new);
+
+
+class CoinPriceViewModel extends AutoDisposeFamilyNotifier<AsyncValue, List<String>> {
+  @override
+  AsyncValue build(List<String> arg) {
+    final  priceStream = ref.watch(coinbasePriceRepositoryProvider(arg));
+    return priceStream;
+  }
+
+  void reset() {
+    ref.invalidate(coinbasePriceRepositoryProvider);
+  }
+  
+}
